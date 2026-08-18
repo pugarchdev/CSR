d6c87d23fae113cf96947342025081899d318dde
Anand Gadge <anandgadge1008@gmail.com>
2026-08-18 12:44:55 +0530

    clening

--- backend/scratch_test.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-  const enquiries = await prisma.corporateEnquiry.findMany();
-  console.log(JSON.stringify(enquiries.map(e => ({ id: e.id, status: e.status, assignedRmId: (e as any).assignedRelationshipManagerId })), null, 2));
-}
-
-main().finally(() => prisma.$disconnect());
